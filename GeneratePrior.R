4f6369a4597f2c7641c4ea27ae9a7bb8092765d7
matthewclark1223 <m.clark@imperial.ac.uk>
2023-11-23 09:48:42 +0000

    plot to make a pseudo prior

--- GeneratePrior.R
+++ GeneratePrior.R
@@ -3,11 +3,11 @@ I0 = 0.02    # initial fraction infected
 S0 = 1 - I0 # initial fraction susceptible
 
 
-# Assign transmission and pathogen-induced death/recovery rates:
+# Assign social spread, drop out and independent adoption rates:
 
-beta = rnorm(1000,0.4,0.02) #rate of infection
-gamma = rnorm(1000,0.15,0.02) #rate of recovery
-alpha =rnorm(1000,0.005,0.0002)#independent rate of infection
+beta = rnorm(1000,0.3,0.02) #rate of social spread
+gamma = rnorm(1000,0.15,0.02) #rate of drop out
+alpha =rnorm(1000,0.02,0.002)#independent rate of adoption
 
 # We will use the package deSolve to integrate, which requires certain data structures.
 # Store parameters and initial values
@@ -20,7 +20,6 @@ params <- list(beta = beta,
 inits <- c(S0, I0) #0 denotes that it is an initial condition
 
 # Create a time series over which to integrate.
-# Here we have an epidemic that is observed over t_max number of days (or weeks or etc).
 t_min = 0
 t_max = 50
 times = t_min:t_max
@@ -30,9 +29,9 @@ times = t_min:t_max
 SISa <- function(t, y, params) {
   with(as.list(c(params, y)), {
     
-    dS = - beta * y[1] * y[2] +gamma * y[2] - alpha*y[1]#y[1] susceptible and y[2] is infected
+    dS = - beta * y[1] * y[2] +gamma * y[2] - alpha*y[1]#y[1] available (S) and y[2] is adopted (I)
     
-    dI = beta * y[1] * y[2] - gamma * y[2] + alpha*y[1]#susceptibles and recovered
+    dI = beta * y[1] * y[2] - gamma * y[2] + alpha*y[1]
     
     res <- c(dS,dI)
     list(res)
@@ -59,7 +58,13 @@ colnames(out) <- c("time", "S", "I","Sample")
 
 #library(ggplot2)
 ggplot(out,aes(x=time,y=I))+
-  geom_line(aes(group=Sample),alpha=0.01)+theme_classic()
+  #geom_line(aes(group=Sample),alpha=0.01)+
+  tidybayes::stat_lineribbon(aes(fill_ramp = after_stat(.width)), .width = ppoints(10), fill = "#045a8d") +#4d004b
+  ggdist::scale_fill_ramp_continuous(range = c(1, 0),name="Credibility interval")+
+  ggthemes::theme_clean()+xlab("Time")+ylab("Predicted adoption")+
+  theme(axis.title = element_text(colour = "black",size=16),
+        axis.text=element_text(color="black",size=14))
+
 
 
 
